620dfc145213fa1522f7d20d109743c4f2bb5b99
Pab <63944729+pabcorn@users.noreply.github.com>
2021-09-21 22:45:11 +0700

    add_algorithm

---
+++ algorithms/fizzbuzz/target/debug/deps/fizzbuzz.d
@@ -0,0 +1,5 @@
+C:\Users\Pabcorn\Desktop\Trainee\kubeops-trainee\algorithms\fizzbuzz\target\debug\deps\fizzbuzz.exe: src\main.rs
+
+C:\Users\Pabcorn\Desktop\Trainee\kubeops-trainee\algorithms\fizzbuzz\target\debug\deps\fizzbuzz.d: src\main.rs
+
+src\main.rs:
---
+++ algorithms/fizzbuzz/target/debug/fizzbuzz.d
@@ -0,0 +1,1 @@
+C:\Users\Pabcorn\Desktop\Trainee\kubeops-trainee\algorithms\fizzbuzz\target\debug\fizzbuzz.exe: C:\Users\Pabcorn\Desktop\Trainee\kubeops-trainee\algorithms\fizzbuzz\src\main.rs
