af6578586b765eb18e601f9da1b68e36ef5d6a74
Pab <63944729+pabcorn@users.noreply.github.com>
2021-09-27 17:07:55 +0700

    add_fizzbuzz

---
+++ algorithms/fizzbuzz.js
@@ -0,0 +1,17 @@
+var fizzBuzz = 3;
+var answer = "";
+
+/* switch (input) {
+    case input % 3:
+        console.log(fizz);
+        break;
+    case input % 5:
+        console.log(buzz);
+        break;
+
+    default:
+        console.log(input);
+        break;
+} */
+answer = fizzBuzz % 3 == 0 & fizzBuzz % 5 == 0 ? console.log("FizzBuzz") : fizzBuzz % 3 == 0 ? console.log("Fizz") : fizzBuzz % 5 == 0 ? console.log("Buzz") : fizzBuzz;
+console.log(answer);
--- algorithms/fizzbuzz/src/main.rs
+++ algorithms/fizzbuzz/src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
-    println!("Hello, world!");
+    let val = 51;
+    let fuzz = String::from("Fuzz");
+    let buzz = String::from("Buzz");
+    let fuzBuz = String::from("Fuzz Buzz");
+    let val = val % 3 == 0 ? fuzz : buzz;
+    println!("Print: {}",val);
 }
