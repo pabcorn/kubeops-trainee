a2bbefccc77407fe3e7346523159d01cf62dfa18
Pab <63944729+pabcorn@users.noreply.github.com>
2021-11-09 22:42:42 +0700

    answer1

--- algorithms/1fizzbuzz.js
+++ algorithms/1fizzbuzz.js
@@ -1,4 +1,4 @@
-var fizzBuzz = 38;
+var fizzBuzz = 15;
 var answer = "";
 
 /* switch (input) {
@@ -14,4 +14,4 @@ var answer = "";
         break;
 } */
 answer = fizzBuzz % 3 === 0 & fizzBuzz % 5 === 0 ? console.log("FizzBuzz") : fizzBuzz % 3 === 0 ? console.log("Fizz") : fizzBuzz % 5 === 0 ? console.log("Buzz") : fizzBuzz;
-console.log(">> Fizzbuzz (" + fizzBuzz +")\n" + answer);
+console.log(">> It's Fizzbuzz Right? (" + fizzBuzz +")");
